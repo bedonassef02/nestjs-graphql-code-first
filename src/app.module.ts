83e76eb48850672a4a26151635d282a6fa6debfa
Bedo <bedonassef71@gmail.com>
2024-10-19 14:13:04 +0300

    connect to pg db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,25 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  }), CoffeesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    CoffeesModule,
+  ],
 })
 export class AppModule {}
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,10 +1,16 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   @Field(() => ID, { description: 'Unique identifier' })
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
+  @Column({ type: 'json' })
   flavors: string[];
 }
